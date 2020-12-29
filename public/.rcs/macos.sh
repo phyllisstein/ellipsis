6d56130f1c6e7f6cc3f536a370e152d6a5bec2ce
Daniel P. Shannon <daniel@daniel.sh>
2020-12-29 13:23:18 -0500

    :wrench: Cleanups.

--- public/.rcs/macos.sh
+++ public/.rcs/macos.sh
@@ -1,6 +1,10 @@
 # ---------------------------------- GraalVM --------------------------------- #
 export GRAAL_JAVA_HOME="/Library/Java/VirtualMachines/graalvm-ee-java11-20.2.0/Contents/Home"
 export GRAAL_PATH="/Library/Java/VirtualMachines/graalvm-ee-java11-20.2.0/Contents/Home/bin"
+export JAVA_HOME="$GRAAL_JAVA_HOME"
 
 # ----------------------------------- iTerm ---------------------------------- #
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+# ------------------------------ macOS CLI Trash ----------------------------- #
+alias rm='trash'
