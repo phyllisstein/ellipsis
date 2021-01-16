ffbf8ba7cdf8c4ecd8887cd24f8146de7fc0ed9b
Daniel P. Shannon <daniel@fleet.i.ng>
2021-01-16 02:42:31 -0500

    🔨 Fix X11 support in Windows.

--- public/.rcs/wsl.sh
+++ public/.rcs/wsl.sh
@@ -9,5 +9,9 @@ export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}";
 export CAROOT="/mnt/c/Users/danie/Dropbox/Settings/SSL"
 export NODE_EXTRA_CA_CERTS="$CAROOT/rootCA.pem"
 
-# ------------------------------- Docker Bridge ------------------------------ #
-export PATH="/mnt/c/ProgramData/DockerDesktop/version-bin:/mnt/c/ProgramFiles/Docker/Docker/resources/bin:$PATH"
+# ------------------------------------ X11 ----------------------------------- #
+wsl_ip_line=$(ipconfig.exe | grep "WSL" -n | awk -F ":" '{print $1+4}')
+export DISPLAY=$(ipconfig.exe | awk -v a=$wsl_ip_line '{if (NR==a) print $NF":0.0"}' | tr -d "\r")
+# export LIBGL_ALWAYS_INDIRECT=1
+# export QT_SCALE_FACTOR=2
+# export GDK_SCALE=2
