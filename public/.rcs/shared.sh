5c32b46d75bb59d65a675bdce5bf755330ad19bf
Daniel P. Shannon <daniel@try.works>
2024-06-21 19:40:54 -0400

    Configs.

--- public/.rcs/shared.sh
+++ public/.rcs/shared.sh
@@ -77,29 +77,6 @@ elif [[ -s "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh" ]]; then
 fi
 
 # ----------------------------------- Pyenv ---------------------------------- #
+export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-PATH="$(bash --norc -ec 'IFS=:; paths=($PATH);
-for i in ${!paths[@]}; do
-if [[ ${paths[i]} == "''/Users/daniel/.pyenv/shims''" ]]; then unset '\''paths[i]'\'';
-fi; done;
-echo "${paths[*]}"')"
-export PATH="/Users/daniel/.pyenv/shims:${PATH}"
-export PYENV_SHELL=zsh
-source '/opt/homebrew/Cellar/pyenv/2.4.3/libexec/../completions/pyenv.zsh'
-command pyenv rehash 2>/dev/null
-pyenv() {
-  local command
-  command="${1:-}"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  rehash|shell)
-    eval "$(pyenv "sh-$command" "$@")"
-    ;;
-  *)
-    command pyenv "$command" "$@"
-    ;;
-  esac
-}
+eval "$(pyenv init -)"
