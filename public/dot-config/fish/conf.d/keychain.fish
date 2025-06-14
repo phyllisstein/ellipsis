4c62b78d4eabca1d83bf1fd27593170b9663519a
Daniel P. Shannon <daniel@try.works>
2025-06-14 05:22:39 -0400

    Load all certs

--- public/dot-config/fish/conf.d/keychain.fish
+++ public/dot-config/fish/conf.d/keychain.fish
@@ -1,1 +1,1 @@
-# keychain --ignore-missing --quiet --eval ~/.ssh/personal_ed25519 ~/.ssh/personal_rsa ~/.ssh/ignota_engineering 2>/dev/null | source
+keychain --ignore-missing --quiet --eval ~/.ssh/personal_ed25519 ~/.ssh/personal_rsa ~/.ssh/ignota_engineering 2>/dev/null | source
